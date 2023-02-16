94e96ec68b62cb5c247fe993fc66627eca48befa
jael137 <jaelkwakkel@gmail.com>
2023-02-16 15:28:04 +0100

    Made ContactFormModel.cs and updated form

--- Setup/Controllers/HomeController.cs
+++ Setup/Controllers/HomeController.cs
@@ -1,61 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using Setup.Models;
-using System.Diagnostics;
 
-namespace Setup.Controllers
+namespace Setup.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private const string PageViews = "PageViews";
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        private const string PageViews = "PageViews";
+    public IActionResult Index()
+    {
+        UpdatePageViewCookie();
+        return View(new HomeModel(Request.Cookies[PageViews]));
+    }
 
-        public HomeController(ILogger<HomeController> logger)
+    [HttpPost]
+    public ActionResult Contact(ContactFormModel input)
+    {
+        if (!ModelState.IsValid) return View(input);
+
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View(new AccountDetails());
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public void UpdatePageViewCookie()
+    {
+        var currentCookieValue = Request.Cookies[PageViews];
+
+        if (currentCookieValue == null)
         {
-            _logger = logger;
+            Response.Cookies.Append(PageViews, "1");
         }
-
-        public IActionResult Index()
+        else
         {
-            UpdatePageViewCookie();
-            return View(new HomeModel(Request.Cookies[PageViews]));
-        }
+            var newCookieValue = short.Parse(currentCookieValue) + 1;
 
-        public IActionResult About()
-        {
-            return View(new AccountDetails());
-        }
-        
-        public IActionResult Contact()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public void UpdatePageViewCookie()
-        {
-            var currentCookieValue = Request.Cookies[PageViews];
-
-            if (currentCookieValue == null)
-            {
-                Response.Cookies.Append(PageViews, "1");
-            }
-            else
-            {
-                var newCookieValue = short.Parse(currentCookieValue) + 1;
-
-                Response.Cookies.Append(PageViews, newCookieValue.ToString());
-            }
+            Response.Cookies.Append(PageViews, newCookieValue.ToString());
         }
     }
 }
---
+++ Setup/Models/ContactFormModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Setup.Models;
+
+[BindProperties]
+public class ContactFormModel
+{
+    [Required] [EmailAddress] public string Email { get; set; }
+
+    [Required]
+    [StringLength(200, ErrorMessage = "The input cannot be longer than 200 characters.")]
+    public string Description { get; set; }
+
+    [Required]
+    [StringLength(600, ErrorMessage = "The input cannot be longer than 600 characters.")]
+    public string Message { get; set; }
+}
