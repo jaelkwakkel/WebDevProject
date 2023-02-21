4a1bbfc667316a76c0fc3d39eeedebb2591cbec8
jael137 <jaelkwakkel@gmail.com>
2023-02-21 20:45:28 +0100

    Added CreateAccount.cshtml to create an account

---
+++ Setup/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Setup.Models.Account;
+
+namespace Setup.Controllers;
+
+public class AccountController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult CreateAccount(AccountModel input)
+    {
+        if (!ModelState.IsValid) return View(input);
+
+        return View();
+    }
+
+    // GET
+    public IActionResult CreateAccount()
+    {
+        return View();
+    }
+}
---
+++ Setup/Models/Account/AccountModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Setup.Models.Account;
+
+[BindProperties]
+public class AccountModel
+{
+    [Key] public long Id { get; set; }
+
+    [Required]
+    [StringLength(12, MinimumLength = 4, ErrorMessage = "You username must be between 4-12 characters.")]
+    public string? Username { get; set; }
+
+    [Required]
+    [EmailAddress(ErrorMessage = "Please use a valid email-address")]
+    public string? Email { get; set; }
+
+    [Required]
+    [PasswordPropertyText]
+    [StringLength(24, MinimumLength = 8, ErrorMessage = "Your password must be between 8-24 characters.")]
+    public string? Password { get; set; }
+
+    [Required]
+    [DisplayName("Confirm password")]
+    [NotMapped] // Does not affect database
+    [Compare("Password", ErrorMessage = "Passwords do not match")]
+    [PasswordPropertyText]
+    public string? ConfirmPassword { get; set; }
+}
