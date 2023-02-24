f9161e29f1d3115b9284b85bf837eabd841b895c
jael137 <jaelkwakkel@gmail.com>
2023-02-24 16:52:17 +0100

    Working lobby connection with sending and receiving messages

---
+++ Setup/Hubs/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using Setup.Models;
+using System.Collections.Concurrent;
+
+namespace Setup.Hubs
+{
+    public static class UserRepository
+    {
+        public static ConcurrentDictionary<string, PlayerModel> Users = new(); //{ get; set; }
+
+        //public UserRepository()
+        //{
+        //Users = new ConcurrentDictionary<string, PlayerModel>();
+        //}
+
+        public static PlayerModel? GetUserById(string id)
+        {
+            if (Users.TryGetValue(id, out PlayerModel? result))
+            {
+                Console.WriteLine("Return user with id: " + id + " and name " + result.Name);
+                return result;
+            }
+            return null;
+        }
+
+        public static void AddUser(PlayerModel user)
+        {
+            Users.TryAdd(user.ConnectionId, user);
+            Console.WriteLine(Users.Count);
+        }
+
+        public static PlayerModel? RemoveUser(string id)
+        {
+            if (Users.TryRemove(id, out PlayerModel? result))
+            {
+                return result;
+            }
+            return null;
+        }
+    }
+}
--- Setup/Program.cs
+++ Setup/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using Setup.Hubs;
 using Setup.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//Add Signalr | https://learn.microsoft.com/en-us/aspnet/core/tutorials/signalr?tabs=visual-studio&WT.mc_id=dotnet-35129-website&view=aspnetcore-7.0
+builder.Services.AddSignalR();
 
 //Get connection string and add DbContext
 var connectionString = builder.Configuration.GetConnectionString("ContactFormDbContext");
@@ -30,5 +33,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     "default",
     "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
